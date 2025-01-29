1a5aa7aefd55ed6ab7a1e4cb6998786b6e4528e9
MAGAYA\aylin.rodriguez <aylin.rodriguez@magaya.com>
2025-01-29 17:32:45 -0500

    Add New TC25 MappingCharges

---
+++ Script/SaveCharge.js
@@ -0,0 +1,11 @@
+﻿function checkAndClickSaveButton() {
+
+    let ocrResult = OCR.Recognize(Aliases.ExpExpl.wndCefBrowserWindow.Chrome_WidgetWin_0.Chrome_RenderWidgetHostHWND);
+ 
+    if (ocrResult.BlockByText("Save").Exists) {
+        Log.Message("'Save' button found. Clicking it.");
+        ocrResult.BlockByText("Save").Click();
+    } else {
+        Log.Message("'Save' button not found. No action performed.");
+    }
+}
