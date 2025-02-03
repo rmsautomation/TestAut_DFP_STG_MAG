eef23b2d32332277d6910740a53cd757d0c008bf
MAGAYA\aylin.rodriguez <aylin.rodriguez@magaya.com>
2025-02-03 13:06:52 -0500

    Update Logout and WR method

--- Script/CloseMagaya.js
+++ Script/CloseMagaya.js
@@ -13,3 +13,23 @@
     Log.Error("The proccess was not closed'ExpExpl.exe': " + e.message);
   }
 }
+
+
+function closeExpExplNew() {
+  try {
+    var appName = "ExpExpl";
+    
+    // Check if the process exists before trying to access it
+    if (Sys.WaitProcess(appName, 1000).Exists) {
+      var process = Sys.Process(appName); // Now it's safe to access the process
+      process.Terminate();
+      Log.Message("Process '" + appName + "' was closed.");
+    } else {
+      Log.Message("Process '" + appName + "' is not running. Continuing test execution.");
+    }
+  } catch (e) {
+    Log.Warning("Could not close process '" + appName + "': " + e.message);
+  }
+}
+
+
