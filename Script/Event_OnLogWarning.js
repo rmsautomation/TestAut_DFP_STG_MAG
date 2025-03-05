1f40ae4e766c1aeecf533f480cc8d3f341c3fe1d
MAGAYA\aylin.rodriguez <aylin.rodriguez@magaya.com>
2025-03-05 15:53:04 -0500

    update warning

--- Script/Event_OnLogWarning.js
+++ Script/Event_OnLogWarning.js
@@ -3,10 +3,12 @@
     var locked = aqString.Find(LogParams.Str, "disabled");
     // Also check if the warning message mentions "The browser is already running"
     var browserRunning = aqString.Find(LogParams.Str, "The browser is already running");
-    var replace = aqString.Find(LogParams.Str, "object was replaced with a similar object");    
+    
+    var replace = aqString.Find(LogParams.Str, "object was replaced with a similar object");   
+    var controlVisible = aqString.Find(LogParams.Str, "The action may be performed incorrectly, because the control is not visible");  
 
     // If the word "disabled" or "The browser is already running" is found
-    if (locked != -1 || browserRunning != -1 || replace != -1) {
+    if (locked != -1 || browserRunning != -1 || replace != -1 || controlVisible != -1) {
         // Block the warning message from being logged
         LogParams.Locked = true;
     } else {
