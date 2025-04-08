61a4bc70c5a461d73bf743ff115b4d6af473defb
MAGAYA\aylin.rodriguez <aylin.rodriguez@magaya.com>
2025-04-08 09:27:09 -0400

    Update Execution Plan with new script to celan the registry in Magaya

---
+++ Script/CleanRegistryMagaya.js
@@ -0,0 +1,17 @@
+﻿function runBatchAsAdmin() {
+  var batPath = Project.Variables.project_path+"Stores\\Files\\cleanRegistryMagaya.bat";
+
+ 
+  // Ejecutar el archivo .bat como administrador usando PowerShell
+  var command = 'powershell.exe';
+  var args = '-Command "Start-Process \'' + batPath + '\' -Verb RunAs"';
+
+  var process = Sys.OleObject("WScript.Shell").Exec(command + ' ' + args);
+
+  // Esperar a que termine (opcional)
+  while (process.Status === 0) {
+    aqUtils.Delay(100);
+  }
+
+  Log.Message("Batch file executed with elevated privileges.");
+}
