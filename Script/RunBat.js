09b1abb6477f11996aa17fa926f3aa8d2c454f5a
MAGAYA\aylin.rodriguez <aylin.rodriguez@magaya.com>
2025-04-21 17:19:34 -0400

    Add OnStartTestCase Hook (Close Explorer)

---
+++ Script/RunBat.js
@@ -0,0 +1,22 @@
+﻿function RunBat() {
+  Project.Variables.project_path = Project.Path;
+  Project.Variables.upload_path = Project.Variables.project_path + "Stores\\Files\\closeExpExpl.bat";
+  Log.Message("Ruta del proyecto: " + Project.Variables.project_path);
+  Log.Message("Ruta del archivo .bat: " + Project.Variables.upload_path);
+  var shell = Sys.OleObject("WScript.Shell");
+  shell.Run('"' + Project.Variables.upload_path + '"', 0, true); 
+}
+
+function Hooks_OnStartTestCase(Sender, StartTestCaseParams)
+{
+  Project.Variables.project_path = Project.Path;
+
+  Project.Variables.upload_path = Project.Variables.project_path + "Stores\\Files\\closeExpExpl.bat";
+
+  Log.Message("Ruta del proyecto: " + Project.Variables.project_path);
+  Log.Message("Ruta del archivo .bat: " + Project.Variables.upload_path);
+
+  var shell = Sys.OleObject("WScript.Shell");
+  shell.Run('"' + Project.Variables.upload_path + '"', 0, true); 
+  
+}
