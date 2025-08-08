f913f37dbef0736c1fd9877c60bd6779c1cab354
MYCLDSTG\38442.stgautousr10 <aylin.rodriguez@magaya.com>
2025-08-08 17:20:59 -0400

    Updating tests after new Magaya V14

---
+++ Script/ClickOnOutgoingSH.js
@@ -0,0 +1,26 @@
+﻿
+function clickShipment(shipmentName) { 
+  var shipmentNameClean = aqString.Replace(shipmentName, " ", "");
+
+  var items = Aliases.ExpExpl.mainWindow.completePanel.panel.outline
+    .FindAllChildren("ObjectType", "OutlineItem", 10);
+
+  for (var i = 0; i < items.length; i++) {
+    // Limpiamos \n, espacios y OutlineItem("...")
+    var nameClean = items[i].Name;
+    nameClean = aqString.Replace(nameClean, "\n", "");
+    nameClean = aqString.Replace(nameClean, "OutlineItem(\"", "");
+    nameClean = aqString.Replace(nameClean, "\")", "");
+    nameClean = aqString.Trim(nameClean);
+
+    if (aqString.Find(nameClean, shipmentNameClean) > -1) {
+      items[i].Click();
+      Log.Message("Clicked on: " + items[i].Name);
+      return;
+    }
+  }
+
+  Log.Message("No OutlineItem found containing: " + shipmentNameClean);
+}
+
+
